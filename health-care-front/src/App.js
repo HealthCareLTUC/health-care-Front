d33e9291733d02e336b5711481a88a1d05389d10
MARWA H <MARWA.HU18@GMAIL.COM>
2023-07-26 06:51:59 +0000

    comp

--- health-care-front/src/App.js
+++ health-care-front/src/App.js
@@ -21,7 +21,6 @@ function App() {
      <Route path='/profile' element={<></>}/>
     
     </Routes>
- <Index />
     </div>
   );
 }
--- health-care-front/src/components/Reserve/Reserve.js
+++ health-care-front/src/components/Reserve/Reserve.js
@@ -1,50 +1,92 @@
-import { useState, useEffect } from 'react';
-import { Card } from 'react-bootstrap';
-
+import React,{useEffect, useState} from 'react'
+import { Card , Table} from 'react-bootstrap';
 function Reserve() {
+  const [doctor, setDoctor] = useState(null);
   const [appointments, setAppointments] = useState([]);
-  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
-  async function getAppointmentData(doctorName) {
+  const handleGetDoctorInfo = async (name) => {
+    setIsLoading(true);
     try {
-      const url = `${process.env.REACT_APP_SERVER_URL}/doctors/${doctorName}`;
-      const response = await fetch(url);
-      const data = await response.json();
-      setAppointments(data);
-      setError('');
-    } catch (error) {
-      setError('Error fetching reserve data');
-    }
-  }
-console.log(appointments);
-  useEffect(() => {
-    getAppointmentData();
-  }, []);
+      const url = `https://healthcare-back.onrender.com/searchDocN`; 
+      let res = await fetch(url);
+      let recivedData = await res.json();
+      setDoctor(recivedData);
 
+      if (url.data.length > 0) {
+        setDoctor(url.data[0]);
+      } else {
+        setDoctor(null);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+    setIsLoading(false);
+  };
+  useEffect(()=>{
+    handleGetDoctorInfo();
+  },[])
+  console.log(doctor);
+  const handleGetAppointments = async (doctorId) => {
+    setIsLoading(true);
+    try {
+      const response = `https://healthcare-back.onrender.com/getAppointment/${doctorId}`;
+      let res = await fetch(response);
+      let recivedData = await res.json();
+      setAppointments(recivedData.data);
+    } catch (error) {
+      console.error(error);
+      // Set an error message state to display to the user
+    }
+    setIsLoading(false);
+  };
   return (
     <div>
-      <h2>Reserve</h2>
-      {error && <div>{error}</div>}
-      {appointments.map((appointment) => (
-        <Card key={appointment.appointment_id}>
-          <Card.Body>
-            <Card.Title>Doctor: {appointment.doctorName}</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">
-              Specialty: {appointment.Specialty}<br/>
-              Location: {appointment.location}<br/>
-              Phone: {appointment.phone}
-            </Card.Subtitle>
-            <Card.Text>
-              Patient: {appointment.patient_name}<br/>
-              Date: {appointment.reservation_date}<br/>
-              Time: {appointment.reservation_time}<br/>
-              Report: {appointment.report}
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      ))}
+      <button onClick={() => handleGetDoctorInfo("Doctor Name")}>
+        Get Doctor Info
+      </button>
+      <button onClick={() => handleGetAppointments(1)}>Get Appointments</button>
+
+      {isLoading && <div>Loading doctor info...</div>}
+      {doctor && (
+        <div>
+          <Card style={{ width: "18rem",height:"200px",marginLeft:"700px" }}>
+              <Card.Body>
+                <Card.Title>Name: {doctor.name}</Card.Title>
+                <Card.Text>Specialty: {doctor.specialty}</Card.Text>
+                <Card.Text>Address: {doctor.address}</Card.Text>
+                <Card.Text>Phone: {doctor.phone}</Card.Text>
+              </Card.Body>
+            </Card>
+            </div>
+      )}
+     {appointments.length > 0 && (
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Appointment ID</th>
+              <th>Patient ID</th>
+              <th>Date</th>
+              <th>Time</th>
+              <th>Report</th>
+            </tr>
+          </thead>
+          <tbody>
+            {appointments.map((appointment) => (
+              <tr key={appointment.appointment_id}>
+                <td>{appointment.appointment_id}</td>
+                <td>{appointment.patient_id}</td>
+                <td>{appointment.reservation_date}</td>
+                <td>{appointment.reservation_time}</td>
+                <td>{appointment.report}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
     </div>
   );
 }
 
-export default Reserve;
+export default Reserve; 
+      
